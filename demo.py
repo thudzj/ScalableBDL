560b616f97c39d4635cff3e41ed8d47949b13434
zhijie <dengzhijiethu@gmail.com>
2020-10-08 13:21:44 +0800

    U

--- demo.py
+++ demo.py
@@ -10,16 +10,6 @@ sys.path.insert(0, './reproduction')
 from dataset.cifar import load_dataset
 from models.wrn import wrn
 
-def acc(model, loader):
-    model.eval()
-    num_true = 0
-    with torch.no_grad():
-        for i, (input, target) in tqdm(enumerate(loader), total=1000):
-            input = input.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
-            num_true += (model(input).argmax(1) == target).int().sum().item()
-    return num_true/float(len(test_loader.dataset))
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
@@ -33,6 +23,9 @@ if __name__ == '__main__':
     train_loader, test_loader = load_dataset(args)
 
     net = wrn(pretrained=True, depth=28, width=10).cuda()
+    for m in net.modules():
+        if m.__class__.__name__.startswith('Dropout'): 
+            m.eval()
     bayesian_net = to_bayesian(net)
 
     mus, psis = [], []
@@ -59,6 +52,7 @@ if __name__ == '__main__':
             mu_optimizer.zero_grad()
             psi_optimizer.zero_grad()
             loss.backward()
+            print("Epoch {}, ite {}, loss {}".format(epoch, i, loss.item()))
             mu_optimizer.step()
             psi_optimizer.step()
     
